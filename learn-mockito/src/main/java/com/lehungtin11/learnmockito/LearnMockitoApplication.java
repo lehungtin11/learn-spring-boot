bb24527edee7f6ed8f849f5bd047088dfdd574a0
lehungtin11 <hungtin640@gmail.com>
2024-04-13 19:20:30 +0700

    Unitest, Mockito...

---
+++ learn-mockito/src/main/java/com/lehungtin11/learnmockito/LearnMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.lehungtin11.learnmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMockitoApplication.class, args);
+	}
+
+}
---
+++ learn-mockito/src/test/java/com/lehungtin11/learnmockito/LearnMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lehungtin11.learnmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
